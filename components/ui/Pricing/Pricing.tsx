ae9f1df677951b9a98d11ed5fe79df1c6dd43b09
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-10 14:00:07 -0700

    Update Pricing.tsx

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -113,43 +113,6 @@ export default function Pricing({ user, products}: Props) {
               Your dream build, your way. Pick your tier and customize to perfection!
             </p>
           </div>
-          <div className="mt-12 space-y-0 sm:mt-16 flex flex-wrap justify-center gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0">
-            {products.map((product) => {
-              const price = product?.prices
-              if (!price) return null;
-              const priceString = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: price.currency!,
-                minimumFractionDigits: 0
-              }).format((price?.unit_amount || 0) / 100);
-              return (
-                <div
-                  key={product.id}
-                >
-                  <div className="p-6">
-                    <h2 className="text-2xl font-semibold leading-6 text-white">
-                      {product.name}
-                    </h2>
-                    <p className="mt-4 text-zinc-300">{product.description}</p>
-                    <p className="mt-8">
-                      <span className="text-5xl font-extrabold white">
-                        {priceString}
-                      </span>
-                    </p>
-                    <Button
-                      variant="slim"
-                      type="button"
-                      loading={priceIdLoading === price.id}
-                      onClick={() => handleStripeCheckout(price)}
-                      className="block w-full py-2 mt-8 text-sm font-semibold text-center text-white rounded-md hover:bg-zinc-900"
-                    >
-                      {'Configure Now'}
-                    </Button>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
           <LogoCloud />
         </div>
       </section>
