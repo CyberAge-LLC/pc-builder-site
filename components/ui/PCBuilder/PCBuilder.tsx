c8c364707261875eadae7a84cdfb7d4afc092e19
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 14:01:04 -0700

    Update PCBuilder.tsx

--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -10,36 +10,56 @@ export default function PCBuilder() {
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <Link href='/CPU'>
-            <Button>Central Processing Unit (CPU)</Button>
-          </Link>
-          <Link href='/GPU'>
-  	        <Button>Graphics Processing Unit (GPU)</Button>
-          </Link>
-          <Link href='/Motherboard'>
-            <Button>Motherboard</Button>
-          </Link>
-          <Link href='/RAM'>
-            <Button>Random Access Memory (RAM)</Button>
-          </Link>
-          <Link href='/Storage'>
-            <Button>Storage</Button>
-          </Link>
-          <Link href='/PSU'>
-            <Button>Power Supply Unit (PSU)</Button>
-          </Link>
-          <Link href='/Cooling'>
-            <Button>Cooling System</Button>
-          </Link>
-          <Link href='/Case'>
-            <Button>Case (Chassis)</Button>
-          </Link>
-          <Link href='/OS'>
-            <Button>Operating System (OS)</Button>
-          </Link>
-          <Link href='/Peripherals'>
-            <Button>Peripherals (Monitor, Keyboad, Mouse, Etc.)</Button>
-          </Link>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/CPU' align-center>
+              <Button>Central Processing Unit (CPU)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/GPU' align-center>
+    	        <Button>Graphics Processing Unit (GPU)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/Motherboard' align-center>
+              <Button>Motherboard</Button>
+            </Link>
+          </div>  
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/RAM'>
+              <Button>Random Access Memory (RAM)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>  
+            <Link href='/Storage'>
+              <Button>Storage</Button>
+            </Link>
+          </div> 
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/PSU'>
+              <Button>Power Supply Unit (PSU)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/Cooling'>
+              <Button>Cooling System</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/Case'>
+              <Button>Case (Chassis)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/OS'>
+              <Button>Operating System (OS)</Button>
+            </Link>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Link href='/Peripherals'>
+              <Button>Peripherals (Monitor, Keyboad, Mouse, Etc.)</Button>
+            </Link>
+          </div>  
         </div>
         <LogoCloud />
       </div>
