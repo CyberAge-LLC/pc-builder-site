7ff537f38560fac53ca7fe74dd785f6fb13c0dd3
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 13:35:56 -0700

    Update PCBuilder.tsx

--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -10,34 +10,34 @@ export default function PCBuilder() {
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <Link href='./PartPicker/CPU/CPU'>
+          <Link href='/PartPicker/CPU/CPU'>
             <Button>Central Processing Unit (CPU)</Button>
           </Link>
-          <Link href='./PartPicker/GPU/GPU'>
+          <Link href='/PartPicker/GPU/GPU'>
   	        <Button>Graphics Processing Unit (GPU)</Button>
           </Link>
-          <Link href='./PartPicker/Motherboard/Motherboard'>
+          <Link href='/PartPicker/Motherboard/Motherboard'>
             <Button>Motherboard</Button>
           </Link>
-          <Link href='./PartPicker/RAM/RAM'>
+          <Link href='/PartPicker/RAM/RAM'>
             <Button>Random Access Memory (RAM)</Button>
           </Link>
-          <Link href='./PartPicker/Storage/Storage'>
+          <Link href='/PartPicker/Storage/Storage'>
             <Button>Storage</Button>
           </Link>
-          <Link href='./PartPicker/PSU/PSU'>
+          <Link href='/PartPicker/PSU/PSU'>
             <Button>Power Supply Unit (PSU)</Button>
           </Link>
-          <Link href='./PartPicker/Cooling/Cooling'>
+          <Link href='/PartPicker/Cooling/Cooling'>
             <Button>Cooling System</Button>
           </Link>
-          <Link href='./PartPicker/Case/Case'>
+          <Link href='/PartPicker/Case/Case'>
             <Button>Case (Chassis)</Button>
           </Link>
-          <Link href='./PartPicker/OS/OS'>
+          <Link href='/PartPicker/OS/OS'>
             <Button>Operating System (OS)</Button>
           </Link>
-          <Link href='./PartPicker/Peripherals/Peripherals'>
+          <Link href='/PartPicker/Peripherals/Peripherals'>
             <Button>Peripherals (Monitor, Keyboad, Mouse, Etc.)</Button>
           </Link>
         </div>
