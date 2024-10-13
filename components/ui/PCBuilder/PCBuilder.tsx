787763ac3d463900351bcf92b039301a9e15a265
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 12:59:21 -0700

    Update PCBuilder.tsx

--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Link from 'next/link';
 import Button from '@/components/ui/Button';
 import cn from 'classnames';
 
@@ -8,43 +9,43 @@ export default function PCBuilder() {
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <Link href={'./PartPicker/CPU'}>
+          <Link href={'./PartPicker/CPU'}/>
           <Button
             label="Central Processing Unit (CPU)"
           />
-          <Link href={'./PartPicker/GPU'}>
+          <Link href={'./PartPicker/GPU'}/>
 	        <Button
             label="Graphics Processing Unit (GPU)"
           />	
-          <Link href={'./PartPicker/Motherboard'}>
+          <Link href={'./PartPicker/Motherboard'}/>
           <Button
             label="Motherboard"
           />
-          <Link href={'./PartPicker/RAM'}>
+          <Link href={'./PartPicker/RAM'}/>
           <Button
             label="Random Access Memory (RAM)"
           />
-          <Link href={'./PartPicker/Storage'}>
-          <<Button
+          <Link href={'./PartPicker/Storage'}/>
+          <Button
             label="Storage (SSD/HDD)"
           />
-          <Link href={'./PartPicker/PSU'}>
+          <Link href={'./PartPicker/PSU'}/>
           <Button
             label="Power Supply Unit (PSU)"
           />
-          <Link href={'./PartPicker/Cooling System'}>
+          <Link href={'./PartPicker/Cooling System'}/>
           <Button
             label="Cooling System"
           />
-          <Link href={'./PartPicker/Case'}>
+          <Link href={'./PartPicker/Case'}/>
           <Button
             label="Case (Chassis)"
           />
-          <Link href={'./PartPicker/OS'}>
+          <Link href={'./PartPicker/OS'}/>
           <Button
             label="Operating System (OS)"
           />
-          <Link href={{'./PartPicker/Peripherals'}}>
+          <Link href={{'./PartPicker/Peripherals'}}/>
           <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
             Peripherals (Monitor, Keyboard, Mouse, etc.)
           </p>
