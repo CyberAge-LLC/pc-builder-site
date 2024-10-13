8431dfaa42468c5c9b88bebbfafda1b36322bebc
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 14:30:09 -0700

    update

--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -11,52 +11,52 @@ export default function PCBuilder() {
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./cpu/CPU" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./cpu" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Central Processing Unit (CPU)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./gpu/GPU" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./gpu" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
     	        <Button>Graphics Processing Unit (GPU)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./motherboard/Motherboard" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./motherboard" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Motherboard</Button>
             </Link>
           </div>  
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./ram/RAM" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./ram" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Random Access Memory (RAM)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>  
-            <Link href="./storage/Storage" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./storage" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Storage</Button>
             </Link>
           </div> 
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./psu/PSU" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./psu" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Power Supply Unit (PSU)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./cooling/Cooling" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./cooling" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Cooling System</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./case/Case" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./case" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Case (Chassis)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./os/OS" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./os" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Operating System (OS)</Button>
             </Link>
           </div>
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh' }}>
-            <Link href="./peripherals/Peripherals" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
+            <Link href="./peripherals" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none' }}>
               <Button>Peripherals (Monitor, Keyboad, Mouse, Etc.)</Button>
             </Link>
           </div>  
