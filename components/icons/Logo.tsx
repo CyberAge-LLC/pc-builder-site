cb4cb24e38c254f8a670a4df96ede9f5184378cc
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 07:04:36 -0700

    Update Logo.tsx

--- components/icons/Logo.tsx
+++ components/icons/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Logo = () => {
   return (
     <Image 
-      src="https://ogsbootxscuhnzosbkuy.supabase.co/storage/v1/object/public/logo/logo.svg?t=2024-10-13T13%3A59%3A05.419Z"
+      src="https://ogsbootxscuhnzosbkuy.supabase.co/storage/v1/object/public/logo/logo.svg?t=2024-10-13T14%3A04%3A09.304Z"
       alt="CYBERAGE" 
       width={16} 
       height={15} 
