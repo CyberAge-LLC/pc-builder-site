45f2f626c80fcdbbfe2b5e13e56316f47a354fc3
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-13 12:44:06 -0700

    Add PartPicker

--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -1,48 +1,52 @@
 'use client';
 
+
+import React from 'react';
+import Link from 'next/link';
 import Button from '@/components/ui/Button';
-import LogoCloud from '@/components/ui/LogoCloud';
-import type { Tables } from '@/types_db';
-import { getStripe } from '@/utils/stripe/client';
-import { checkoutWithStripe } from '@/utils/stripe/server';
-import { getErrorRedirect } from '@/utils/helpers';
-import { User } from '@supabase/supabase-js';
-import cn from 'classnames';
-import { useRouter, usePathname } from 'next/navigation';
-import { useState } from 'react';
 
 export default function PCBuilder() {
   return (
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Central Processing Unit (CPU)
-          </p>
-	  <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Graphics Processing Unit (GPU)
-          </p> 	
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Motherboard
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Random Access Memory (RAM)
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Storage (HDD/SSD)
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Power Supply Unit (PSU)
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Cooling System
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Case (Chassis)
-          </p>
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
-            Operating System (OS)
-          </p>
+          <Link href={'./PartPicker/CPU'}>
+          <Button
+            label="Central Processing Unit (CPU)"
+          />
+          <Link href={'./PartPicker/GPU'}>
+	        <Button
+            label="Graphics Processing Unit (GPU)"
+          />	
+          <Link href={'./PartPicker/Motherboard'}>
+          <Button
+            label="Motherboard"
+          />
+          <Link href={'./PartPicker/RAM'}>
+          <Button
+            label="Random Access Memory (RAM)"
+          />
+          <Link href={'./PartPicker/Storage'}>
+          <<Button
+            label="Storage (SSD/HDD)"
+          />
+          <Link href={'./PartPicker/PSU'}>
+          <Button
+            label="Power Supply Unit (PSU)"
+          />
+          <Link href={'./PartPicker/Cooling System'}>
+          <Button
+            label="Cooling System"
+          />
+          <Link href={'./PartPicker/Case'}>
+          <Button
+            label="Case (Chassis)"
+          />
+          <Link href={'./PartPicker/OS'}>
+          <Button
+            label="Operating System (OS)"
+          />
+          <Link href={{'./PartPicker/Peripherals'}}>
           <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
             Peripherals (Monitor, Keyboard, Mouse, etc.)
           </p>
