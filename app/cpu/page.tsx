8d04b7ebb2c809a25152063bd71ae94536464c58
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-14 00:55:27 -0700

    Update page.tsx

--- app/cpu/page.tsx
+++ app/cpu/page.tsx
@@ -30,7 +30,7 @@ export default function CPUTable() {
   const [data, setData] = useState<TableRow[]>([]);
   const [page, setPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
-  const rowsPerPage = 20; // Define how many rows per page
+  const rowsPerPage = 50; // Define how many rows per page
   const [sortConfig, setSortConfig] = useState<{ key: keyof TableRow; direction: 'ascending' | 'descending' } | null>(null);
   const [loading, setLoading] = useState(true); // Loading state
 
@@ -77,6 +77,7 @@ export default function CPUTable() {
 
   const handlePageClick = (event: PageClickEvent) => {
     setPage(event.selected);
+    await fetchData();
   };
 
   const handleSort = (key: keyof TableRow) => {
