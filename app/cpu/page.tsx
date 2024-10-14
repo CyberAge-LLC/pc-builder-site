26d24fa420f0a47d00bc90b8ef8d29af6d623f18
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-14 09:58:21 -0700

    Update

--- app/cpu/page.tsx
+++ app/cpu/page.tsx
@@ -122,9 +122,9 @@ export default function CPUTable() {
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
-          <p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+          <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
             Select CPU
-          </p>
+          </h1>
         </div>
 
         <div>
--- components/ui/PCBuilder/PCBuilder.tsx
+++ components/ui/PCBuilder/PCBuilder.tsx
@@ -9,6 +9,9 @@ export default function PCBuilder() {
   return (
     <section className="bg-black">
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
+        <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+          PC Builder
+        </h1>
         <div className="sm:flex sm:flex-col sm:align-center">
           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '8vh', width: '100%' }}>
             <Link href="./cpu" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', textDecoration: 'none', width: '100%' }}>
