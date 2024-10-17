a868e209e5a59455050698194aa52ea4221d8c26
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-16 18:44:49 -0700

    Update page.tsx

--- app/case/page.tsx
+++ app/case/page.tsx
@@ -119,7 +119,7 @@ export default function CaseTable() {
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Select Case
+            Select Chassis
           </h1>
         </div>
 
