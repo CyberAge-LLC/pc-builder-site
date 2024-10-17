0286bc5e19699d5b272aae2ca180800a6a0244bb
William Averill <112809324+cyberagellc@users.noreply.github.com>
2024-10-16 18:42:37 -0700

    Update

--- app/ram/page.tsx
+++ app/ram/page.tsx
@@ -153,7 +153,7 @@ export default function MemoryTable() {
       <div className="max-w-6xl px-4 py-8 mx-auto sm:py-24 sm:px-6 lg:px-8">
         <div className="sm:flex sm:flex-col sm:align-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
-            Select Memory
+            Select RAM
           </h1>
         </div>
 
